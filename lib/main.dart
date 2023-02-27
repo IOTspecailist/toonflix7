bcad26175fd08b9999304f7d43949d7af2105f89
nobody <asdf@asdffasdassadsad.com>
2023-02-27 22:36:58 +0900

    nomad소스읽기

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,13 @@ class App extends StatelessWidget {
             horizontal: 40,
           ),
           child: Column(
+            // column 위젯 팝업?툴박스 보면 변수명 옆에 = 있는건 설정안할시 자동 설정되는 초기값임
+            // 그래서아래서 verticalDirection이 없어도 에러안남
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              //children은 위젯list로 선언되어있음 그래서 대괄호로 묶음
               SizedBox(
-                height: 80,
+                height: 40,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -28,7 +32,7 @@ class App extends StatelessWidget {
                       Text(
                         'Hey, Selena',
                         style: TextStyle(
-                          color: Colors.white,
+                          color: Color.fromARGB(255, 236, 50, 50),
                           fontSize: 28,
                           fontWeight: FontWeight.w800,
                         ),
@@ -36,11 +40,57 @@ class App extends StatelessWidget {
                       Text(
                         'Welcome back',
                         style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.8),
+                          color: Color.fromRGBO(30, 195, 69, 0.8),
                           fontSize: 18,
                         ),
                       ),
                     ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
                   )
                 ],
               )
