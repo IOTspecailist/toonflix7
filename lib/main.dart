3f7efe0e7eae5b86322d3318c927b201f48860c0
nobody <asdf@asdffasdassadsad.com>
2023-03-04 21:48:29 +0900

    소스마저 분석 완료

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,37 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     //빌드 위젯을 오버라이드
     return MaterialApp(
+      // 위젯의 시작과 끝은 괄호다
       // 리턴에 위젯을 선언해버리네
       //이 맨처음 위젯이 맨아래 깔리는 땅 같은 느낌이고
       // 그다음 scaffold라는 위젯을 깔고 그위에 padding 위젯깔고 이런 느낌이다
       home: Scaffold(
         //home은 변수인데 타입이 위젯이라 scaffold 위젯으로 초기화
-        backgroundColor: const Color.fromARGB(255, 132, 44, 44),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
         //Scaffold 위젯의 변수인 backgroundColor 는 color타입 변수
         body: Padding(
-          //
+          //backgroundColor와 body 둘다 Scaffold 위젯의 변수지만
+          // body는 자료형이 위젯이라 왼쪽에 하위클래스 라인이 생겼다
           padding: const EdgeInsets.symmetric(
             horizontal: 25,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            //컬럼위젯을 개발자도구로 보면 폰 전체 세로범위를 나타낸다
+            //컬럼전체 align을 왼쪽(start)으로 했지만 서브위젯에서
+            //오른쪽으로 하면 컬럼 내에서 오른쪽으로 (시릴)가네
             children: [
+              //List<위젯> 자료형인 children
               const SizedBox(
+                //그냥 박스네 nico눈 박스크기를 가지고 화면내부 글자위치를 조정함
                 height: 80,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
+                  //children은 List<위젯>이니까 대괄호로 시작
                   Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: const [
                       Text(
                         'Hey, Selena',
@@ -67,7 +75,7 @@ class App extends StatelessWidget {
                 height: 5,
               ),
               const Text(
-                '\$5 194 482',
+                '\$858,194,482',
                 style: TextStyle(
                   fontSize: 48,
                   fontWeight: FontWeight.w600,
@@ -87,7 +95,7 @@ class App extends StatelessWidget {
                   ),
                   Button(
                     text: 'Request',
-                    bgColor: Color(0xFF1F2123),
+                    bgColor: Color(0xFFF1B33B),
                     textColor: Colors.white,
                   ),
                 ],
@@ -110,7 +118,7 @@ class App extends StatelessWidget {
                   Text(
                     'View All',
                     style: TextStyle(
-                      color: Colors.white.withOpacity(0.8),
+                      color: Colors.white.withOpacity(0.6),
                       fontSize: 18,
                     ),
                   ),
@@ -120,10 +128,14 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                //유로 이미지 카드는 컨테이너로 담았군
+                //이미지는 컨테이너인가
                 clipBehavior: Clip.hardEdge,
+                //이 부분을 바꾸면 유로 로고가 컨테이너 범위를 초과한다
+
                 decoration: BoxDecoration(
-                  color: const Color(0xFF1F2123),
-                  borderRadius: BorderRadius.circular(25),
+                  color: const Color.fromARGB(255, 27, 108, 189),
+                  borderRadius: BorderRadius.circular(25), //모서리라운드
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(30),
@@ -147,7 +159,7 @@ class App extends StatelessWidget {
                           Row(
                             children: [
                               const Text(
-                                '6 428',
+                                '799,848,004',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 20,
@@ -172,7 +184,7 @@ class App extends StatelessWidget {
                         child: Transform.translate(
                           offset: const Offset(-5, 12),
                           child: const Icon(
-                            Icons.euro_rounded,
+                            Icons.euro_rounded, //유로모양
                             color: Colors.white,
                             size: 88,
                           ),
